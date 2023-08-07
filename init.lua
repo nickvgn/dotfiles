02aff98356e40b7e6efd9b7c343005c07dbaeb50
NALURI-NICK <naluri-nick@NALURI-NICKs-MacBook-Pro.local>
2023-08-07 08:55:17 +0800

    fix: remove twilight nvim

--- init.lua
+++ init.lua
@@ -762,29 +762,6 @@ require("lazy").setup({
 			}
 		end,
 	},
-	{
-		"folke/twilight.nvim",
-		-- keys ={"<space>tw" },
-		event = "VeryLazy",
-		config = function()
-			require("twilight").setup({
-				dimming = {
-					alpha = 0.4, -- amount of dimming
-				},
-				context = 15, -- amount of lines we will try to show around the current line
-				-- treesitter is used to automatically expand the visible text,
-				-- but you can further control the types of nodes that should always be fully expanded
-				expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-					"function",
-					"method",
-					"table",
-					"if_statement",
-				},
-				exclude = {}, -- exclude these filetypes
-			})
-			vim.cmd(":Twilight")
-		end,
-	},
 }, {})
 
 -- [[ Setting options ]]

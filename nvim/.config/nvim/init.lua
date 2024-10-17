f49b46419f819d4c2ad78f58002ac569f8c95d8e
nickvgn <nick.von42@gmail.com>
2024-10-17 09:41:32 +0800

    feat: traverse quickfix list

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -132,5 +132,32 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("n", "]q", "<cmd>cnext<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "[q", "<cmd>cprev<cr>", { noremap = true, silent = true })
 
+local function remove_quickfix_item()
+	local qf_list = vim.fn.getqflist()
+
+	local qf_info = vim.fn.getqflist({ idx = 0 })
+	local current_idx = qf_info.idx
+
+	-- If the list is not empty and there is a valid index
+	if current_idx > 0 and current_idx <= #qf_list then
+		-- Remove the current entry from the quickfix list
+		table.remove(qf_list, current_idx)
+
+		-- Update the quickfix list with the modified list
+		vim.fn.setqflist(qf_list, "r")
+
+		-- Set the quickfix list to the next item, or the previous if the last item was removed
+		local next_idx = math.min(current_idx, #qf_list) -- Ensure we don't exceed the list size
+		if next_idx > 0 then
+			vim.fn.setqflist({}, "r", { idx = next_idx })
+
+			-- Jump to the next quickfix item (changes the code buffer)
+			vim.cmd("cc") -- This is like typing :cc to jump to the current quickfix entry
+		end
+	end
+end
+
+vim.keymap.set("n", "<leader>dq", remove_quickfix_item, { noremap = true, silent = true })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -586,10 +586,39 @@ return {
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { "biome-check" },
-				typescript = { "biome-check" },
-				typescriptreact = { "biome-check" },
+				javascript = { "biome" },
+				javascriptreact = { "biome" },
+				typescript = { "biome" },
+				typescriptreact = { "biome" },
+				json = { "biome" },
+				jsonc = { "biome" },
 			},
+			-- Set this to change the default values when calling conform.format()
+			-- This will also affect the default values for format_on_save/format_after_save
+			-- default_format_opts = {
+			-- 	lsp_format = "fallback",
+			-- },
+			-- If this is set, Conform will run the formatter on save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			-- format_on_save = {
+			-- 	-- I recommend these options. See :help conform.format for details.
+			-- 	lsp_format = "fallback",
+			-- 	timeout_ms = 500,
+			-- },
+			-- If this is set, Conform will run the formatter asynchronously after save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			-- format_after_save = {
+			-- 	lsp_format = "fallback",
+			-- },
+			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+			log_level = vim.log.levels.ERROR,
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
+			-- Custom formatters and overrides for built-in formatters
 		},
 	},
 	-- Experimental stuff

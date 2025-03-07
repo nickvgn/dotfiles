0d7d5d3d4cd6294608b118637486d3d76b8ae394
nickvgn <nick.von42@gmail.com>
2025-03-07 08:34:51 +0800

    feat: conform fuzzy rust

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"saghen/blink.cmp",
+		event = "VeryLazy",
 		-- optional: provides snippets for the snippet source
 		dependencies = "rafamadriz/friendly-snippets",
 		-- use a release tag to download pre-built binaries
@@ -47,6 +48,13 @@ return {
 
 			-- experimental signature help support
 			signature = { enabled = true },
+			
+    -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
